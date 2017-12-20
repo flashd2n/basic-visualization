2595235509260bd313196e88555e5a736dd611ba
flashd2n <k.kostov.eu@gmail.com>
2017-12-20 07:08:49 +0200

    prepared for dynamic visualization

--- start.js
+++ start.js
@@ -5,16 +5,13 @@ const path = require('path');
 const filePath = path.join(__dirname, './work.json');
 
 fs.readFile(filePath, 'UTF-8', (err, data) => {
-    console.log(typeof data);
     const json = JSON.parse(data);
-    console.log(typeof json);
 
     const compiledFunc = pug.compileFile('./templates/container.pug');
     const output = compiledFunc({
         stats: json.stats,
         tests: json.tests
     });
-    console.log(output);
 
     const outputPath = path.join(__dirname, './outout.html');
 
